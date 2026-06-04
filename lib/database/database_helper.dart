631b4ec5e5f7690599f8557d9aad728cb4e8968a
jatukr-dotcom <jatukr-dotcom@users.noreply.github.com>
2026-06-04 17:51:38 +0800

    fix: tambah import flutter/foundation untuk debugPrint di database_helper

--- lib/database/database_helper.dart
+++ lib/database/database_helper.dart
@@ -1,6 +1,7 @@
 // =========================================================
 // database/database_helper.dart - Helper SQLite lokal
 // =========================================================
+import 'package:flutter/foundation.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
